8738d996beb672cb9cfae534d2e4a0b69560ba11
Joris Noordermeer <joris@noordermeer.ch>
2019-12-06 17:42:40 +0100

    Add noopener tag to links

--- src/js/components/ExternalLogos.vue
+++ src/js/components/ExternalLogos.vue
@@ -14,8 +14,17 @@
 <template>
     <div>
         <ul class="externalLogos">
-            <li v-for="link in links" class="externalLogos-entry" :class="'externalLogos-entry--' + link.title.toLowerCase().replace(' ', '')">
-                <a class="externalLogos-link" :href="link.url" target="_blank"></a>
+            <li
+                v-for="link in links"
+                class="externalLogos-entry"
+                :class="'externalLogos-entry--' + link.title.toLowerCase().replace(' ', '')"
+            >
+                <a
+                    class="externalLogos-link"
+                    :href="link.url"
+                    target="_blank"
+                    rel="noopener"
+                ></a>
             </li>
         </ul>
     </div>
--- src/js/components/FooterLinks.vue
+++ src/js/components/FooterLinks.vue
@@ -19,10 +19,13 @@
 <!--                {{ link.title }} <a :href="(link.email) ? 'mailto:' + link.email : link.url" target="_blank" class="footerLinks-link">{{ (link.text) ? link.text : ((link.email) ? link.email : link.url) }}</a>-->
 <!--            </li>-->
             <li class="footerLinks-entry" >
-                Booking <a href="mailto:kilian@orangepeel-agency.ch" target="_blank" class="footerLinks-link">kilian@orangepeel-agency.ch</a>
+                Booking
+                <a href="mailto:kilian@orangepeel-agency.ch" target="_blank" class="footerLinks-link">kilian@orangepeel-agency.ch</a>
             </li>
             <li class="footerLinks-entry" >
-                Website By <a href="mailto:joris@noordermeer.ch" target="_blank" class="footerLinks-link">Joris Noordermeer (Code)</a> <a href="https://rubenfeurer.com" target="_blank" class="footerLinks-link">Ruben Feurer (Design)</a>
+                Website By
+                <a href="mailto:joris@noordermeer.ch" target="_blank" class="footerLinks-link">Joris Noordermeer (Code)</a>
+                <a href="https://rubenfeurer.com" target="_blank" class="footerLinks-link" rel="noopener">Ruben Feurer (Design)</a>
             </li>
             <li class="footerLinks-entry" >
                 Management <a href="mailto:rene.renner@lmeworldwide.com" target="_blank" class="footerLinks-link">rene.renner@lmeworldwide.com</a>
